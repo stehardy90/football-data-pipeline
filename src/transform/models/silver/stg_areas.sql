5f1edf677b94a6960ba620bcff69aed4fce388bb
Steven Hardy <stehardy90@gmail.com>
2024-11-01 19:46:46 +0000

    Updating dbt models

--- src/transform/models/silver/stg_areas.sql
+++ src/transform/models/silver/stg_areas.sql
@@ -1,17 +1,34 @@
-with raw_areas as (
-    select distinct
+WITH raw_competitions AS (
+	SELECT * FROM {{ source('football_data_pipeline','raw_football_competitions') }}
+)
+
+,areas_stage AS (
+    
+	SELECT DISTINCT
         JSON_EXTRACT_SCALAR(raw_json, '$.area.code') AS area_code,
         JSON_EXTRACT_SCALAR(raw_json, '$.area.name') AS area_name,
         JSON_EXTRACT_SCALAR(raw_json, '$.area.flag') AS area_flag,
-        row_number() over (partition by JSON_EXTRACT_SCALAR(raw_json, '$.area.code') order by loaded_date desc) as row_num
-    from `{{ var('bigquery_dataset') }}.raw_football_competitions`
-	WHERE JSON_EXTRACT_SCALAR(raw_json, '$.area.code') IS NOT NULL
+        ROW_NUMBER() OVER (PARTITION BY JSON_EXTRACT_SCALAR(raw_json, '$.area.code') ORDER BY loaded_date DESC) AS row_num
+    FROM 
+		raw_competitions
+	WHERE 
+		JSON_EXTRACT_SCALAR(raw_json, '$.area.code') IS NOT NULL
+		
 )
 
-select
-	area_code,
-	area_name,
-	area_flag,
-	CURRENT_TIMESTAMP() AS loaded_date
-from raw_areas
-where row_num = 1
+,final AS (
+	
+	SELECT
+		area_code,
+		area_name,
+		area_flag,
+		CURRENT_TIMESTAMP() AS loaded_date
+	FROM 
+		areas_stage
+	WHERE 
+		row_num = 1
+
+)
+
+
+SELECT * FROM final
