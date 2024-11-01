53960059993685a020112e1aa54cb8a29551a7ed
Steven Hardy <stehardy90@gmail.com>
2024-11-01 20:17:11 +0000

    Updating dbt models

--- src/transform/models/silver/stg_coaches.sql
+++ src/transform/models/silver/stg_coaches.sql
@@ -1,5 +1,12 @@
-WITH raw_coaches AS (
-    SELECT
+WITH raw_teams AS (
+
+	SELECT * FROM {{ source('football_data_pipeline','raw_football_teams') }}
+	
+)
+
+,coaches_stage AS (
+    
+	SELECT
         CAST(JSON_EXTRACT_SCALAR(team_data, '$.id') AS INT64) AS team_id,
         CAST(JSON_EXTRACT_SCALAR(coach_data, '$.id') AS INT64) AS coach_id,
         JSON_EXTRACT_SCALAR(coach_data, '$.name') AS coach_name,
@@ -19,21 +26,34 @@ WITH raw_coaches AS (
         END AS coach_contract_until,
 
         ROW_NUMBER() OVER (PARTITION BY JSON_EXTRACT_SCALAR(team_data, '$.id') ORDER BY loaded_date DESC) AS row_num
-    FROM `{{ var('bigquery_dataset') }}.raw_football_teams`,
-    UNNEST(JSON_EXTRACT_ARRAY(raw_json, '$.teams')) AS team_data,  -- Unnest the teams array first
-    UNNEST([JSON_EXTRACT(team_data, '$.coach')]) AS coach_data  -- Extract the coach field from each team
-		WHERE JSON_EXTRACT_SCALAR(team_data, '$.id') IS NOT NULL
+    
+	FROM 
+		raw_teams,
+		UNNEST(JSON_EXTRACT_ARRAY(raw_json, '$.teams')) AS team_data,  -- Unnest the teams array first
+		UNNEST([JSON_EXTRACT(team_data, '$.coach')]) AS coach_data  -- Extract the coach field from each team
+	
+	WHERE 
+		JSON_EXTRACT_SCALAR(team_data, '$.id') IS NOT NULL
+		
 )
 
-SELECT
-    coach_id,
-    team_id,
-    coach_name,
-    coach_date_of_birth,
-    coach_nationality,
-    coach_contract_start,
-    coach_contract_until,
-    CURRENT_TIMESTAMP() AS loaded_date
-FROM raw_coaches
-WHERE row_num = 1
-AND coach_id is not null
+,final as (
+
+	SELECT
+		coach_id,
+		team_id,
+		coach_name,
+		coach_date_of_birth,
+		coach_nationality,
+		coach_contract_start,
+		coach_contract_until,
+		CURRENT_TIMESTAMP() AS loaded_date
+	FROM 
+		coaches_stage
+	WHERE 
+		row_num = 1
+		AND coach_id is not null
+
+)
+
+SELECT * FROM final
